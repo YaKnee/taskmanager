b440c8d669653e8855fc105eeebd4c2d2019eb76
YaKnee <jani.oc@hotmail.com>
2024-12-15 18:06:36 +0000

    refactor: change validation so that name is required for post and put

--- middlewares/validateTask.js
+++ middlewares/validateTask.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
 const taskValidationSchema = Joi.object({
-    name: Joi.string().min(3).optional(), // Name is optional for update and at least 3 characters long
+    name: Joi.string().min(3).required(), // Name must be at least 3 characters long
     dueDate: Joi.date().greater(new Date()).optional(), // Optional dueDate, can be a future date
     completed: Joi.boolean().default(false), // Completed is optional and defaults to false
     priority: Joi.string().valid("None", "Low", "Medium", "High").default("Low") // Priority must be one of the allowed values
-});
+}).min(1);
 
 export const validateTask = (req, res, next) => {
     const { error } = taskValidationSchema.validate(req.body, { abortEarly: false });
